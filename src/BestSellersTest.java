21d0e1e8d6559cd04a162e76dd32b03f2a2a22e2
gauravsharma05 <gauravsharma05@gmail.com>
2024-07-06 13:33:30 +0100

    Update BestSellersTest.java

--- src/BestSellersTest.java
+++ src/BestSellersTest.java
@@ -1,7 +1,11 @@
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -18,24 +22,36 @@ public class BestSellersTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.amazon.co.uk");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+
 		
 		driver.findElement(By.id("sp-cc-accept")).click();
 		
 		
+		
 		//click on Best Seller
+		
 		driver.findElement(By.linkText("Best Sellers")).click();
+
+		clickCookieButton(driver);
+	
+		
 		
 		BestSellersPage BestSeller = new BestSellersPage();
 		
 		BestSeller.Amazon_Devices_and_Accessories(driver).click();
 		driver.navigate().back();
 		
+		clickCookieButton(driver);
+		
+		
 		BestSeller.clickAmazon_Devices_and_Accessories(driver);
 		driver.navigate().back();
+	     
+		clickCookieButton(driver);
 		
-		BestSeller.clickAmazon_Launchpad(driver);
-		driver.navigate().back();
+	//	BestSeller.clickAmazon_Launchpad(driver);
+	//	driver.navigate().back();
+	
 		
 		BestSeller.clickApps_and_Games(driver);
 		driver.navigate().back();
@@ -44,5 +60,15 @@ public class BestSellersTest {
 		driver.quit();
 		
 	}
+	
+	
+	// Method to click on the cookie button
+	public static void clickCookieButton(WebDriver driver) {
+	List<WebElement> z = driver.findElements(By.id("sp-cc-accept"));
 
+	if (z.size() > 0) {
+		driver.findElement(By.id("sp-cc-accept")).click();
+	}
+	}
 }
+
